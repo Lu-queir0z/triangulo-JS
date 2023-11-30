cdae36cb95e643e37bd9849487c02c8f67a94824
ls1w <michelle_j.b@hotmail.com>
2023-11-30 09:36:13 -0300

    tudo ok

--- calcular.js
+++ calcular.js
@@ -5,53 +5,32 @@ let numero3;
 let resultado;
 
 function triangulo(){
+
     numero1 = parseInt(document.querySelector('#NUMERO1').value)
     numero2 = parseInt(document.querySelector('#NUMERO2').value)
     numero3 = parseInt(document.querySelector('#NUMERO3').value)
 
-    if (numero1 == numero2 && numero2 ==  numero3 ) {
-        resultado = 'triangulo equilatero'
-    }
-    else if(numero1 ==  numero2 != numero3 ) {
-        resultado = 'triangulo isoceles'
-    }else {
-        resultado = 'triangulo escaleno'
-    }
+    let imgTriangulo = document.querySelector('#imgTriangulo')
 
+    if((numero1 == numero2) && (numero1 == numero3)){
+        resultado = 'triangulo Equilatero'
+        imgTriangulo.setAttribute('src','img/equilatero.png');
+    }else if ((numero1 != numero2) && (numero1 != numero3) && (numero2 != numero3)){
+    resultado = 'Triangulo Escaleno'
+    imgTriangulo.setAttribute('src','img/escaleno.png');
+    }else{
+        resultado = 'Triangulo Isoceles'
+        imgTriangulo.setAttribute('src','img/isoceles.png');
+    }
+    
     document.querySelector('#resultado').innerHTML = resultado;
+    
 }
-/*
-//tratamento das imagens dos operadores
-let select = document.querySelector('#slcOperador');
-
-select.addEventListener('change',function(){
-
-    let imgOperacao = document.querySelector('#imgOperacao');
-    let op = select.options[select.selectedIndex].value;
-
-    if(op == '+'){
-        imgOperacao.setAttribute('src','img calculadora/adicionar.png');
-    }
-
-    if(op == '-'){
-        imgOperacao.setAttribute('src','img calculadora/subtracao.png');
-    }
-
-    if(op == '*'){
-        imgOperacao.setAttribute('src','img calculadora/multiplicacao.png');
-    }
-
-    if(op == '/'){
-        imgOperacao.setAttribute('src','img calculadora/divisao.png');
-    }
-});
-*/
 
 //funçao de limpar formulario
 function limpar(){
-
     document.querySelector('#NUMERO1').value = '';
     document.querySelector('#NUMERO2').value = '';
+    document.querySelector('#NUMERO3').value = '';
     document.querySelector('#resultado').innerHTML = '-';
-    
 }
